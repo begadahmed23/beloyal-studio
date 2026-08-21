d4640f11208ab721a6044d786a9f285f3b4989fb
begad ahmed <begad.ahmed124@gmail.com>
2026-08-21 16:27:58 +0300

    feat: add barber ratings and join poster

--- app/card/[token]/page.tsx
+++ app/card/[token]/page.tsx
@@ -1332,6 +1332,7 @@ const progressPercentage = rewardReady
 
       {showGooglePrompt ? (
         <GoogleReviewModal
+          businessType={customer.cafe.businessType}
           selectedRating={
             selectedRating
           }
